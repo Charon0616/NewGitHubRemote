633e9b40b23803ea75a9dc382137cc563298d52c
Davi Ji <v-weji1@microsoft.com>
2022-03-16 10:02:20 +0000

    fasf

--- ConsoleApp0806TestRepo/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-//hello
-//chff
-//vdquickadfddhsfgsfnnasdfazB03
-//vdquickadfddghjkghjasdfasdfnnassdfasdfaasasdfgassdfasdfdfazB03
