7551a56531b7df0ff20358085e9e6ea5639a265c
Charon0616 <85989668+Charon0616@users.noreply.github.com>
2022-05-05 17:48:21 +0800

    Update Program (2).cs

--- ConsoleApp0806TestRepo/Program (2).cs
+++ ConsoleApp0806TestRepo/Program (2).cs
@@ -4,6 +4,7 @@ Console.WriteLine("Hello, World!");
 //hello
 //chaneg
 //vhnhndfdddjhjahsjdf
+//New55
 Console.WriteLine("Hello namsfffsssse World!");
             Console.WriteLine("Hello thhellois World!");
             Console.WriteLine("Hello the fffWorld!");
